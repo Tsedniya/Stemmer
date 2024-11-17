8adbe5dcb4deeed66371e01a675973b7b2ba2adf
Tsedniya Frezewed <ktsedniya@gmail.com>
2024-11-17 13:34:23 +0400

    Project Added

---
+++ Controllers/ExchangeRateController.cs
@@ -0,0 +1,35 @@
+﻿using BanksExchangeRates.Domain.Entities;
+using BanksExchangeRates.Domain.Interfaces;
+using BanksExchangeRates.Infrastructure.Repositories;
+using BanksExchangeRates.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace BanksExchangeRates.Controllers
+{
+    public class ExchangeRateController : Controller
+    {
+        private readonly IOptions<List<XPathModel>> _options;
+        private readonly IHubContext<MyHub> _hubContext;
+        private readonly IDataService _dataService;
+        public ExchangeRateController(IOptions<List<XPathModel>> options, IHubContext<MyHub> hubContext, IDataService dataService)
+        {
+            _options = options;
+            _hubContext = hubContext;
+            _dataService = dataService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public async Task<string> FetchInitialData()
+        {
+            var data = await _dataService.GetUpdatedDataAsync();
+            return JsonSerializer.Serialize(data);
+        }
+    }
+}
---
+++ Infrastructure/Repositories/FetchWebPage.cs
@@ -0,0 +1,20 @@
+﻿using BanksExchangeRates.Domain.Interfaces;
+
+namespace BanksExchangeRates.Infrastructure.Repositories
+{
+    public class FetchWebPage : IFetchWebPage
+    {
+        public async Task<string> FecthWebPage(string url)
+        {
+            using (var _httpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/html"));
+
+                HttpResponseMessage response = await _httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
